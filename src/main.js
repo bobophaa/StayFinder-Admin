3ad2181606fd56300fae740e6dcdf127f3b145a8
sieitha15 <sieitha4@gmail.com>
2026-03-26 14:51:46 +0700

    Push utils

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import './assets/style.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+//toast
+import 'vue-toast-notification/dist/theme-sugar.css';
+
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App.vue'
 import router from './router'
